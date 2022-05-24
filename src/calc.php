8aa5bc7497b8a0687ba098592c8c375e6f879423
SantaRenata <renatasanta2002@gmail.com>
2022-05-24 09:48:40 +0200

    Bemenő adatok érvényessége vizsgálat

--- src/calc.php
+++ src/calc.php
@@ -2,11 +2,6 @@
 echo file_get_contents("templates/head.html");
 
 $page = file_get_contents("templates/calc.html");
-$aside = $_GET['aside'];
-$bside = $_GET['bside'];
-$cside = $_GET['cside'];
-$dside = $_GET['dside'];
-
 function calcArea($aside,$bside,$cside,$dside){
     $s = ($aside+$bside+$cside+$dside)/2;
     $area = sqrt(
@@ -17,7 +12,16 @@ function calcArea($aside,$bside,$cside,$dside){
     );
     return $area;    
 }
-$area = calcArea($aside,$bside,$cside,$dside);
+if (!empty($_GET['aside']) and !empty($_GET['bside']) and !empty($_GET['cside']) and !empty($_GET['dside'])){
+    $aside = $_GET['aside'];
+    $bside = $_GET['bside'];
+    $cside = $_GET['cside'];
+    $dside = $_GET['dside'];
+    $area = calcArea($aside,$bside,$cside,$dside);
+}else {
+    $area = "Hiba! Helytelen bemenő adatok.";
+}
+
 $page = str_replace("{{result}}",$area,$page);
 
 echo $page;
